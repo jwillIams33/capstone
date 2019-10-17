7016dee0da3c5e5e585e54722891262f6355313a
Williams <jamal.williams@wwt.com>
2019-10-17 11:02:43 -0500

    added pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <section>
+<h2>logo</h2>
+<h1>Stay Colorful</h1>
+<p>Featuring artfully designed guest rooms and suites, ours is the first hotel in the world that gives you the options of booking your room not only by bed, but also invited to experience an emotion of color – Passion Red, Tranquility Blue, Rejuvenation Green or Happiness Yellow.</p>
+</section>
+    )
+}
+
+export default IndexPage
+
---
+++ src/pages/passionRedRoom.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const passionRedPage = () => {
+    return (
+        <div>Passion Red Room</div>
+    )
+}
+
+export default passionRedPage
