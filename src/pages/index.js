0ea9e289a3aed1e8a8af6bab6550da3b42457150
Williams <jamal.williams@wwt.com>
2019-10-17 11:09:56 -0500

    Added links to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
     return (
@@ -6,6 +7,14 @@ const IndexPage = () => {
 <h2>logo</h2>
 <h1>Stay Colorful</h1>
 <p>Featuring artfully designed guest rooms and suites, ours is the first hotel in the world that gives you the options of booking your room not only by bed, but also invited to experience an emotion of color – Passion Red, Tranquility Blue, Rejuvenation Green or Happiness Yellow.</p>
+<ul>
+<li><Link>Red</Link></li>
+<li><Link>Blue</Link></li>
+<li><Link>Green</Link></li>
+<li><Link>Yellow</Link></li>
+
+
+</ul>
 </section>
     )
 }
