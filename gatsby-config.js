9a25af2d5e2267b58517db49b6246b879dfabb85
Williams <jamal.williams@wwt.com>
2019-10-17 16:49:52 -0500

    modified config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,29 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images`,
+        name: 'images'
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+      }
+    },
+    'gatsby-plugin-offline'
+  ]
 }
