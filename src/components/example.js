1fa793a2f7b1b0db969be6e63f0b6d2f775aab7b
Williams <jamal.williams@wwt.com>
2019-10-25 15:19:56 -0500

    modified the datepicker component

--- src/components/example.js
+++ src/components/example.js
@@ -2,25 +2,27 @@ import React, { useState } from "react"
 import DatePicker from "react-datepicker"
 import flexboxGrid from "./flexboxGrid.module.css"
 import Column from "./column"
-
+import Row from "./row"
 
 import "react-datepicker/dist/react-datepicker.css"
 
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-const Example = () => {
+const ChooseDate = props => {
   const [startDate, setStartDate] = useState(null)
   return (
-    <Column>
-                <DatePicker 
-    selected={startDate} 
-    onChange={date => setStartDate(date)}
-    placeholderText="mm/dd/yyyy"
-     />
-    </Column>
-   
+    <div>
+      <Row>
+        <label>{props.label}</label>
+      </Row>
+      <DatePicker
+        selected={startDate}
+        onChange={date => setStartDate(date)}
+        placeholderText="mm/dd/yyyy"
+      />
+    </div>
   )
 }
 
-export default Example
+export default ChooseDate
--- src/pages/redPage.js
+++ src/pages/redPage.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Example from "../components/example"
+import ChooseDate from "../components/example"
 import Footer from "../components/footer"
 import Hero from "../components/hero"
 import ImgGrid from "../components/imgGrid"
@@ -7,6 +7,9 @@ import SectionContent from "../components/sectionContent"
 import Header from "../components/header"
 import Art from "../components/art"
 import Rooms from "../data/rooms"
+import Row from "../components/row"
+import Column from "../components/column"
+import Wrapper from "../components/wrapper"
 
 import flexboxGrid from "../components/flexboxGrid.module.css"
 
@@ -16,56 +19,75 @@ import blueQueen from "../images/blue/blue_queen.jpg"
 import DatePicker from "react-datepicker"
 
 import Booking from "../components/booking"
- 
-import "react-datepicker/dist/react-datepicker.css";
+
+import "react-datepicker/dist/react-datepicker.css"
 
 const bg = {
-    backgroundImage: "url(" + Art.passionRed + ")",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    width: "100%",
-    height: "100%",
-    overflow: "hidden",
-  }
-
+  backgroundImage: "url(" + Art.passionRed + ")",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  width: "100%",
+  height: "100%",
+  overflow: "hidden",
+}
 
 const redPage = () => {
+  return (
+    <div>
+      <Header />
 
+      <Hero style={bg} title={Rooms.colors.red} />
 
-    return (
-        <div>
-        <Header />
-      
-        <Hero style ={bg} title= {Rooms.colors.red}  />
-        {/* <Booking /> */}
-        <div className={flexboxGrid.datepicker}>
-          <div className={flexboxGrid.dateContainer}>
-            {/* <p>Check-In</p> */}
-            <Example />
-          </div>
-          <div className={flexboxGrid.dateContainer}><Example /></div>
-          <div className={flexboxGrid.dateContainer}>
-            <select>
-  <option value="volvo">1 Guest</option>
-  <option value="saab">2 Guests</option>
-  <option value="opel">3 Guests</option>
-  <option value="audi">4 Guests</option>
-</select>
-<select>
-  <option value="volvo">1 Room</option>
-  <option value="saab">2 Rooms</option>
-  <option value="opel">3 Rooms</option>
-  <option value="audi">4 Rooms</option>
-</select>
-</div>
-      
+      <Wrapper>
+        <Row>
+          <ChooseDate label="Check-In" />
+          <ChooseDate label="Check-Out" />
+          <ChooseDate label="Check-In" />
+          <ChooseDate label="Check-Out" />
+          <ChooseDate label="Check-Out" />
+        </Row>
+
+        {/* <div className={flexboxGrid.dateContainer}>
+          <Row>
+            <label for="pet-select">Guests</label>
+          </Row>
+
+          <select name="pets" className={flexboxGrid.guests}>
+            <option value="">1 Guest</option>
+            <option value="dog">2 Guests</option>
+            <option value="cat">Cat</option>
+            <option value="hamster">Hamster</option>
+            <option value="parrot">Parrot</option>
+            <option value="spider">Spider</option>
+            <option value="goldfish">Goldfish</option>
+          </select>
         </div>
-        <SectionContent heading = {Rooms.colors.red} />
-        <ImgGrid />
-        <Footer />
-      </div>
-    )
+        <div className={flexboxGrid.dateContainer}>
+          <Row>
+            <label for="pet-select">Rooms</label>
+          </Row>
+
+          <select name="pets" className={flexboxGrid.guests}>
+            <option value="">1 Room</option>
+            <option value="dog">2 Rooms</option>
+            <option value="cat">Cat</option>
+            <option value="hamster">Hamster</option>
+            <option value="parrot">Parrot</option>
+            <option value="spider">Spider</option>
+            <option value="goldfish">Goldfish</option>
+          </select>
+        </div>
+        <div className={flexboxGrid.dateContainer}>
+          <button>Check Availability</button>
+        </div> */}
+      </Wrapper>
+
+      <SectionContent heading={Rooms.colors.red} />
+      <ImgGrid />
+      <Footer />
+    </div>
+  )
 }
 
 export default redPage
