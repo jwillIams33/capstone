44572324158a2f131f96379dee4fdff459cace98
Williams <jamal.williams@wwt.com>
2019-10-31 17:20:01 -0500

    got the responsive nav working

--- src/components/example-2.js
+++ src/components/example-2.js
@@ -3,22 +3,40 @@ import gridTest from "../pages/newgrid.module.css"
 
 const ResponsiveHeader = props => {
   const [isToggled, setToggle] = useState(false)
+ 
 
-  let itemsClass = gridTest.items;
+  let itemsClass = gridTest.items
+  let nav = gridTest.nav
+  let menu = "Menu"
 
   if (isToggled) {
-    itemsClass += "is-toggled";
+    nav += "is-toggled";
+    menu = "Close"
   }
 
   return (
     <>
-      <button onClick={() => setToggle(!isToggled)} className={gridTest.menu}>
-        Menu
-      </button>
-      <header className={itemsClass}>
-        <div className={gridTest.child}>child</div>
-        <div className={gridTest.child}>child</div>
-        <div className={gridTest.child}>child</div>
+      <header>
+        <div className={gridTest.row}>
+          <h2 className={gridTest.logo}>Logo</h2>
+          <button
+            onClick={() => setToggle(!isToggled)}
+            className={gridTest.menu}
+          >
+           {menu}
+          </button>
+        </div>
+
+        <div className={itemsClass}>
+          <h2 className={gridTest.logo2}>Logo</h2>
+
+          <ul className={nav}>
+            <li className={gridTest.child}>Child</li>
+            <li className={gridTest.child}>Child</li>
+            <li className={gridTest.child}>Child</li>
+            <li className={gridTest.child}>Child</li>
+          </ul>
+        </div>
       </header>
     </>
   )
