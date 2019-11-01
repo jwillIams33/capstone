60cb8d10be0f8302f5b9c06188118cdf98679108
Williams <jamal.williams@wwt.com>
2019-11-01 10:57:32 -0500

    bJ messed up my code

--- src/components/example-2.js
+++ src/components/example-2.js
@@ -3,33 +3,39 @@ import gridTest from "../pages/newgrid.module.css"
 
 const ResponsiveHeader = props => {
   const [isToggled, setToggle] = useState(false)
- 
 
   let itemsClass = gridTest.items
   let nav = gridTest.nav
   let menu = "Menu"
 
+  let buttonItemsClass = itemsClass + " button"
+
   if (isToggled) {
-    nav += "is-toggled";
+    nav += "is-toggled"
     menu = "Close"
   }
 
   return (
     <>
       <header>
-        <div className={gridTest.row}>
+        {/* <div className={gridTest.row}>
           <h2 className={gridTest.logo}>Logo</h2>
           <button
             onClick={() => setToggle(!isToggled)}
             className={gridTest.menu}
           >
-           {menu}
+            {menu}
           </button>
-        </div>
+        </div> */}
 
         <div className={itemsClass}>
           <h2 className={gridTest.logo2}>Logo</h2>
-
+          <button
+            onClick={() => setToggle(!isToggled)}
+            className={gridTest.menu}
+          >
+            {menu}
+          </button>
           <ul className={nav}>
             <li className={gridTest.child}>Child</li>
             <li className={gridTest.child}>Child</li>
@@ -38,6 +44,13 @@ const ResponsiveHeader = props => {
           </ul>
         </div>
       </header>
+      <main>
+        <p>
+          sadlkjsdaf lkasdjf lkasdjflkasdjflk;asdjfkl;asdjfkljadsf
+          asdsadfasdfasdfasdf asd fasdf asdf adsf adsf asdf asdfasdf asdf asdf
+          asd fadsf dsaf dsaf adsf adf adfsasdf da aasdfsad fadsf.
+        </p>
+      </main>
     </>
   )
 }
