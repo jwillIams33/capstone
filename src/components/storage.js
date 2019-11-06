8aaae599b2db30db37f529e3de824b2545c7700b
Williams <jamal.williams@wwt.com>
2019-11-06 12:13:10 -0600

    minor update

--- src/components/storage.js
+++ src/components/storage.js
@@ -1,12 +1,12 @@
 const storage = {
     get: (key) => {
-        if(typeof window.localStorage !== undefined){
+        if(typeof window !== undefined){
           let value = window.localStorage.getItem(key)
           return value
         }
     },
     set: (key, value) => {
-         if(typeof window.localStorage !== undefined){
+         if(typeof window !== undefined){
                 window.localStorage.setItem(key, value)
             }
         }
